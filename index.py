34cdcafbe7a63ea9aa68082540014008afbb921e
Gouda_Clark <mclarkuga@gmail.com>
2020-05-06 21:12:38 -0400

    streamlined code, labeled graphs

--- index.py
+++ index.py
@@ -10,13 +10,9 @@ from dash.dependencies import Input, Output
 import os
 import dash_bootstrap_components as dbc
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
 
-us_airlines = ['ALK', 'DAL', 'LUV', 'SAVE']
 airlines = ['ALK', 'AVH', 'AZUL', 'CEA', 'ZNH', 'VLRS', 'DAL', 'GOL', 'LTM', 'LUV', 'SAVE']
 al_data = yf.download("ALK AVH AZUL CEA ZNH VLRS DAL GOL LTM LUV SAVE", start="2017-01-01", end="2021-05-03")
 df = pd.DataFrame(data=al_data)
@@ -26,9 +22,11 @@ fig = go.Figure()
 def add_lines(strategy):
     for company in strategy:
         fig.add_trace(go.Scatter(x=acdf.index,y=acdf[company], mode='lines', name=company))
-
+        fig.update_layout(             
+            xaxis=dict(title="NYSE Ticker"),
+            yaxis=dict(title="Adj Close")
+        )
 add_lines(airlines)
-# add_lines(us_airlines)
 
 def plot_event(event_date):
     change_list = []
@@ -37,7 +35,6 @@ def plot_event(event_date):
         us_case_close = acdf[company][(acdf.index == event_date)][0]
         percent_change = ((last_close - us_case_close)/(us_case_close))*100
         change_list.append(percent_change)
-    print('Change List = {}'.format(change_list))
     return {
         'data': [
             {'x': airlines, 'y':change_list, 'type':'bar'}
@@ -49,18 +46,6 @@ def plot_event(event_date):
             
         }
     } 
-
-us_fig = plot_event('2020-01-21')
-ch_block = plot_event('2020-01-31')
-com_spread = plot_event('2020-02-26')
-em_bill = plot_event('2020-03-04')
-eur_ban = plot_event('2020-03-11')
-nat_em = plot_event('2020-03-11')
-sp_bill = plot_event('2020-03-27')
-tot_auth = plot_event('2020-04-13')
-bh_sold = plot_event('2020-05-01')
-
-
 
 app.layout = html.Div([
     dbc.Navbar([
@@ -74,10 +59,7 @@ app.layout = html.Div([
             ),
             href="http://pandemair.herokuapp.com/",
         ),
-        # dbc.NavbarToggler(id="navbar-toggler"),
     ], color="#4e455d", dark=True, fixed='top'),
-
-    # html.H1('Airline Ticker Tracker', style = {'background-color': '#4e455d', 'color':'white'}),
     html.H4('Airline stocks on the NYSE closing price since 2017', style={'margin-top': '15vh', 'textAlign':'center'}),
     dcc.Graph(
         id='airline-graph',
@@ -95,9 +77,8 @@ app.layout = html.Div([
         {'label': 'Mar 27: Trump signs $2.2 trillion emergency spending bill', 'value': '2020-03-27'},
         {'label': 'Apr 13: Trump claims "total" authority over governors on openings', 'value': '2020-04-13'},
         {'label': 'May 2: Buffet announces Berkshire Hathaway sold all airline stock', 'value': '2020-05-01'},
-    ], value='2020-01-01', style = {'margin': '0 auto', 'max-width':'90%'}
+    ], value='2020-01-02', style = {'margin': '0 auto', 'max-width':'90%'}
 ),
-    # html.H5(id='event_title', style= {'textAlign':'center', 'margin-top': '40px'}),
     html.Div(id='event_graph'),
 ])
 
@@ -105,27 +86,7 @@ app.layout = html.Div([
     Output('event_graph', 'children'),
     [Input('select_event', 'value')])
 def callback_a(selected_event):
-    if selected_event == '2020-01-21':
-        return dcc.Graph(figure=us_fig)
-    elif selected_event == '2020-01-31':
-        return dcc.Graph(figure=ch_block)
-    elif selected_event == '2020-02-26':
-        return dcc.Graph(figure=com_spread)
-    elif selected_event == '2020-03-04':
-        return dcc.Graph(figure=em_bill)
-    elif selected_event == '2020-03-11':
-        return dcc.Graph(figure=eur_ban)
-    elif selected_event == '2020-03-13':
-        return dcc.Graph(figure=nat_em)
-    elif selected_event == '2020-03-27':
-        return dcc.Graph(figure=ch_block)
-    elif selected_event == '2020-04-13':
-        return dcc.Graph(figure=tot_auth)
-    elif selected_event == '2020-05-02':
-        return dcc.Graph(figure=bh_sold)
-    else:
-        return dcc.Graph(figure=us_fig)
-
+    return dcc.Graph(figure=plot_event(selected_event))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
